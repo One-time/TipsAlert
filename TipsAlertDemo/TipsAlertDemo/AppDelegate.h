8b28729aba7ebf762d7d25fb12aa838d4a66caa7
jack <xueyi.mr@hotmail.com>
2016-03-11 15:13:39 +0800

    first

---
+++ TipsAlertDemo/TipsAlertDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipsAlertDemo
+//
+//  Created by Jack on 16/3/11.
+//  Copyright © 2016年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
